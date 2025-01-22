01b585fa4ba4e1a19a5aeb0d3dad856402d46a53
Ash Monsh <wh7r.com@gmail.com>
2025-01-22 11:48:54 +0300

    fix overflow

--- resources/views/components/accordion/index.blade.php
+++ resources/views/components/accordion/index.blade.php
@@ -7,7 +7,7 @@
             this.activeAccordion = (this.activeAccordion == id) ? '' : id
         }
     }"
-     class="fi-accordion rounded-xl bg-white shadow-sm ring-1 ring-gray-950/5 dark:bg-gray-900 dark:ring-white/10 relative w-full mx-auto overflow-hidden divide-y divide-gray-200 dark:divide-white/5"
+     class="fi-accordion rounded-xl bg-white shadow-sm ring-1 ring-gray-950/5 dark:bg-gray-900 dark:ring-white/10 relative w-full mx-auto divide-y divide-gray-200 dark:divide-white/5"
 >
     {{ $slot }}
 </div>
